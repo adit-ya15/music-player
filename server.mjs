66fee6c63cf311cb1390d038aa1bc589927a31e8
adit-ya15 <aditya262701@gmail.com>
2026-03-04 17:59:08 +0530

    fix the server file

--- server.mjs
+++ server.mjs
@@ -346,8 +346,8 @@ function parseDuration(text) {
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../dist/index.html"));
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 // ─────────────────────────────────────────────
